96db1319dbc9b8899148e7018795b121ad600a95
Leidy Villegas <leja0812@hotmail.com>
2017-02-25 09:14:31 -0500

    Se definieron nuevas rutas

--- app/Http/Controllers/SitioController.php
+++ app/Http/Controllers/SitioController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class SitioController extends Controller
 {
-    //
+   public function index()
+   {
+    echo "Hola Mundo";
+   }
+    public function saludar(Request $request, $nombre)
+    {
+    echo "Hello " . $nombre;
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hola/index',
+'SitioController@index');
+
+Route::get('hola/saludo/{nombre}',
+'SitioController@saludar');
